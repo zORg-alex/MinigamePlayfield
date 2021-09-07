e2cded53519d910571f1aa55a27767cfd2e04020
davidburlaka <davidburlaka99@gmail.com>
2021-09-07 22:23:18 +0300

    Added pop and push to ItemContainer

---
+++ Assets/ItemObject.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "item", menuName = "ScriptableObjects/ItemObject", order = 1)]
+public class ItemObject : ScriptableObject {
+	public Texture2D texture;
+	public string name;
+	public string description;
+}
